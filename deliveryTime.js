6b694bcb864ff8753b6fc987ad1f1a8f041e3e7a
Ricki Moore <rickimoore@Rickis-MacBook-Air.local>
2017-01-07 13:28:28 +0100

    added instructions to deliveryTime code challenge

--- deliveryTime.js
+++ deliveryTime.js
@@ -1,3 +1,20 @@
+/*
+
+Can Santa save Christmas?
+
+Oh no! Santa's little elves are sick this year. He has to distribute the presents on his own. 
+But he has only 24 hours left. Can he do it?
+Your job is to determine if Santa can distribute all the presents in 24 hours. 
+
+Your Task:
+
+You will get an array as input with time durations as string in the following format: 
+"hh:mm:ss"
+
+Return true or false whether he can do it or not. 
+
+*/
+
 function determineTime(durations){
  var count = durations.length;
 
